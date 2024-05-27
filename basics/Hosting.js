171eebe2755b18e3650e8dba5212ffcc8274a150
Sivareddy0930 <sivareddy0930@gmail.com>
2024-05-27 18:31:30 +0530

    js

--- basics/Hosting.js
+++ basics/Hosting.js
@@ -1,10 +1,11 @@
-// var a=5;
-// var b=5;
-// console.log(`     ${a===b}`)
-// console.log("" + a===b)
-
 debugger;
-let arr=[1,2,3,4,5,6]
+console.log(a)
+var a=5;
+console.log(a)
 
-let sum=arr.reduce((acc,currentvalue)=> acc +=currentvalue ,0)
-console.log(sum)
+
+// debugger;
+// let arr=[1,2,3,4,5,6]
+
+// let sum=arr.reduce((acc,currentvalue)=> acc +=currentvalue ,0)
+// console.log(sum)
